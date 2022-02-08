6c210af859b4b30de2ad40bb8867adbe5c7caa97
Yub95 <96728997+Yub95@users.noreply.github.com>
2022-02-09 01:54:53 +0900

    Update Swing.py

--- Swing.py
+++ Swing.py
@@ -54,92 +54,86 @@ while True:
         start_time = get_start_time("KRW-ETH")  #9:00
         end_time = start_time + datetime.timedelta(days=1)  #9:00 + 1일
         
-        if end_time - datetime.timedelta(minutes=130) < now < end_time - datetime.timedelta(seconds = 30) :
+        #매도 06:40 ~ 08:59:30
+        if end_time - datetime.timedelta(minutes=140) < now < end_time - datetime.timedelta(seconds = 30) :
             krw = get_balance("KRW")
-        else:
-        #ETH
-            if start_time < now < end_time - datetime.timedelta(minutes=140):
-                eth = get_balance("ETH")
-                eth_target_price = get_target_price("KRW-ETH", 0.4)
-                eth_target_amount = krw*0.1995 / eth_target_price
-                eth_ma5 = get_ma5("KRW-ETH")
-                eth_current_price = get_current_price("KRW-ETH")
-                eth_start_price = get_start_price("KRW-ETH")
-                if eth_target_price < eth_current_price < eth_target_price*1.0018 and eth_ma5 < eth_start_price:
-                    if krw > 5000 and eth < 0.000001 and upbit.get_order("KRW-ETH","wait") == []:
-                        upbit.buy_limit_order("KRW-ETH",eth_target_price, eth_target_amount)
-            else:
-                eth = get_balance("ETH")
-                if eth > 0.00000001:
-                    upbit.sell_market_order("KRW-ETH", eth)
+            eth = get_balance("ETH")
+            btc = get_balance("BTC")
+            ada = get_balance("ADA")
+            sand = get_balance("SAND")
+            sol = get_balance("SOL")
 
-        #BTC
-            if start_time < now < end_time - datetime.timedelta(minutes=140):
-                btc = get_balance("BTC")
-                btc_target_price = get_target_price("KRW-BTC", 0.4)
-                btc_target_amount = krw*0.1995 / btc_target_price
-                btc_ma5 = get_ma5("KRW-BTC")
-                btc_current_price = get_current_price("KRW-BTC")
-                btc_start_price = get_start_price("KRW-BTC")
-                if btc_target_price < btc_current_price <btc_target_price*1.0002 and btc_ma5 < btc_start_price:
-                    if krw > 5000 and btc < 0.000001 and upbit.get_order("KRW-BTC","wait") == []:
-                        upbit.buy_limit_order("KRW-BTC",btc_target_price, btc_target_amount)
-            else:
-                btc = get_balance("BTC")
-                if btc > 0.00000001:
-                    upbit.sell_market_order("KRW-BTC", btc)
-
-        #ADA
-            if start_time < now < end_time - datetime.timedelta(minutes=140):
-                ada = get_balance("ADA")
-                ada_target_price = get_target_price("KRW-ADA", 0.5)
-                ada_target_amount = krw*0.1995 / ada_target_price
-                ada_ma5 = get_ma5("KRW-ADA")
-                ada_current_price = get_current_price("KRW-ADA")
-                ada_start_price = get_start_price("KRW-ADA")
-                if ada_target_price < ada_current_price < ada_target_price*1.015 and ada_ma5 < ada_start_price:
-                    if krw > 5000 and ada < 0.1 and upbit.get_order("KRW-ADA","wait") == []:
-                        upbit.buy_limit_order("KRW-ADA",ada_target_price, ada_target_amount)
-            else:
-                ada = get_balance("ADA")
-                if ada > 0.00000001:
-                    upbit.sell_market_order("KRW-ADA", ada)
+            if eth > 0.00000001:
+                upbit.sell_market_order("KRW-ETH", eth)
+            if btc > 0.00000001:
+                upbit.sell_market_order("KRW-BTC", btc)
+            if ada > 0.00000001:
+                upbit.sell_market_order("KRW-ADA", ada)  
+            if sand > 0.00000001:
+                upbit.sell_market_order("KRW-SAND", sand)
+            if sol > 0.00000001:
+                upbit.sell_market_order("KRW-SOL", sol)
 
 
-        #SAND
-            if start_time < now < end_time - datetime.timedelta(minutes=140):
-                sand = get_balance("SAND")
-                sand_target_price = get_target_price("KRW-SAND", 0.5)
-                sand_target_amount = krw*0.1995 / sand_target_price
-                sand_ma5 = get_ma5("KRW-SAND")
-                sand_current_price = get_current_price("KRW-SAND")
-                sand_start_price = get_start_price("KRW-SAND")
-                if sand_target_price < sand_current_price < sand_target_price*1.0035 and sand_ma5 < sand_start_price:
-                    if krw > 5000 and sand < 1 and upbit.get_order("KRW-SAND","wait") == []:
-                        upbit.buy_limit_order("KRW-SAND",sand_target_price, sand_target_amount)
-            else:
-                sand = get_balance("SAND")
-                if sand > 0.00000001:
-                    upbit.sell_market_order("KRW-SAND", sand)
+        #매수 09:00 ~ 06:39
+        if start_time < now < end_time - datetime.timedelta(minutes=141):
+            #ETH
+            eth = get_balance("ETH")
+            eth_target_price = get_target_price("KRW-ETH", 0.4)
+            eth_target_amount = krw*0.1995 / eth_target_price
+            eth_ma5 = get_ma5("KRW-ETH")
+            eth_current_price = get_current_price("KRW-ETH")
+            eth_start_price = get_start_price("KRW-ETH")
+            if eth_target_price < eth_current_price < eth_target_price*1.0018 and eth_ma5 < eth_start_price:
+                if krw > 5000 and eth < 0.000001 and upbit.get_order("KRW-ETH","wait") == []:
+                    upbit.buy_limit_order("KRW-ETH",eth_target_price, eth_target_amount)
+
+            #BTC
+            btc = get_balance("BTC")
+            btc_target_price = get_target_price("KRW-BTC", 0.4)
+            btc_target_amount = krw*0.1995 / btc_target_price
+            btc_ma5 = get_ma5("KRW-BTC")
+            btc_current_price = get_current_price("KRW-BTC")
+            btc_start_price = get_start_price("KRW-BTC")
+            if btc_target_price < btc_current_price <btc_target_price*1.0002 and btc_ma5 < btc_start_price:
+                if krw > 5000 and btc < 0.000001 and upbit.get_order("KRW-BTC","wait") == []:
+                    upbit.buy_limit_order("KRW-BTC",btc_target_price, btc_target_amount)
+
+            #ADA
+            ada = get_balance("ADA")
+            ada_target_price = get_target_price("KRW-ADA", 0.5)
+            ada_target_amount = krw*0.1995 / ada_target_price
+            ada_ma5 = get_ma5("KRW-ADA")
+            ada_current_price = get_current_price("KRW-ADA")
+            ada_start_price = get_start_price("KRW-ADA")
+            if ada_target_price < ada_current_price < ada_target_price*1.015 and ada_ma5 < ada_start_price:
+                if krw > 5000 and ada < 0.1 and upbit.get_order("KRW-ADA","wait") == []:
+                    upbit.buy_limit_order("KRW-ADA",ada_target_price, ada_target_amount)
 
 
-        #SOL
-            if start_time < now < end_time - datetime.timedelta(minutes=140):
-                sol = get_balance("SOL")
-                sol_target_price = get_target_price("KRW-SOL", 0.5)
-                sol_target_amount = krw*0.1995 / sol_target_price
-                sol_ma5 = get_ma5("KRW-SOL")
-                sol_current_price = get_current_price("KRW-SOL")
-                sol_start_price = get_start_price("KRW-SOL")
-                if sol_target_price < sol_current_price <sol_target_price*1.002 and sol_ma5 < sol_start_price:
-                    if krw > 5000 and sol < 0.001 and upbit.get_order("KRW-SOL","wait") == []:
-                        upbit.buy_limit_order("KRW-SOL",sol_target_price, sol_target_amount)
-            else:
-                sol = get_balance("SOL")
-                if sol > 0.00000001:
-                    upbit.sell_market_order("KRW-SOL", sol)
+            #SAND
+            sand = get_balance("SAND")
+            sand_target_price = get_target_price("KRW-SAND", 0.5)
+            sand_target_amount = krw*0.1995 / sand_target_price
+            sand_ma5 = get_ma5("KRW-SAND")
+            sand_current_price = get_current_price("KRW-SAND")
+            sand_start_price = get_start_price("KRW-SAND")
+            if sand_target_price < sand_current_price < sand_target_price*1.0035 and sand_ma5 < sand_start_price:
+                if krw > 5000 and sand < 1 and upbit.get_order("KRW-SAND","wait") == []:
+                    upbit.buy_limit_order("KRW-SAND",sand_target_price, sand_target_amount)
 
-            time.sleep(1)
+            #SOL
+            sol = get_balance("SOL")
+            sol_target_price = get_target_price("KRW-SOL", 0.5)
+            sol_target_amount = krw*0.1995 / sol_target_price
+            sol_ma5 = get_ma5("KRW-SOL")
+            sol_current_price = get_current_price("KRW-SOL")
+            sol_start_price = get_start_price("KRW-SOL")
+            if sol_target_price < sol_current_price <sol_target_price*1.002 and sol_ma5 < sol_start_price:
+                if krw > 5000 and sol < 0.001 and upbit.get_order("KRW-SOL","wait") == []:
+                    upbit.buy_limit_order("KRW-SOL",sol_target_price, sol_target_amount)
+
+        time.sleep(1)
         
     except Exception as e:
         print(e)
